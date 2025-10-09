db5471e97a8d4dddfbc28c689f85da68b738ac64
mestepa12 <mestepa120504@gmail.com>
2025-10-09 03:12:41 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// Este archivo contiene la configuración secreta de Firebase.
+// ¡NO LO SUBAS A GITHUB!
+
+const firebaseConfig = {
+  apiKey: "AIzaSyARsawsH2ZFS0yYh5aYg8r1yrUgLFvaX18", // Pega aquí tu nueva clave
+  authDomain: "mi-rincon-de-lectura.firebaseapp.com",
+  projectId: "mi-rincon-de-lectura",
+  storageBucket: "mi-rincon-de-lectura.firebasestorage.app",
+  messagingSenderId: "333643518949",
+  appId: "1:333643518949:web:322ec9b7ab1c3bc267d50c"
+};
