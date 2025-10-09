22d782019497a70510a6baeedda5a61d1fd1e530
mestepa12 <mestepa120504@gmail.com>
2025-10-09 03:42:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
-// Este archivo contiene la configuración secreta de Firebase.
-// ¡NO LO SUBAS A GITHUB!
-
 const firebaseConfig = {
-  apiKey: "AIzaSyARsawsH2ZFS0yYh5aYg8r1yrUgLFvaX18", // Pega aquí tu nueva clave
+  apiKey: "AIzaSyDGgrJwBRmz5hAqkgx3A6CnNRZuR_YtLfc",
   authDomain: "mi-rincon-de-lectura.firebaseapp.com",
   projectId: "mi-rincon-de-lectura",
   storageBucket: "mi-rincon-de-lectura.firebasestorage.app",
