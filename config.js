9a5487653d82d29d95b9e9a207acea34bcd2513f
Miguel Estepa <mestepa120504@gmail.com>
2025-10-09 03:37:51 +0200

    clave API

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDGgrJwBRmz5hAqkgx3A6CnNRZuR_YtLfc",
+  authDomain: "mi-rincon-de-lectura.firebaseapp.com",
+  projectId: "mi-rincon-de-lectura",
+  storageBucket: "mi-rincon-de-lectura.firebasestorage.app",
+  messagingSenderId: "333643518949",
+  appId: "1:333643518949:web:322ec9b7ab1c3bc267d50c"
+};
