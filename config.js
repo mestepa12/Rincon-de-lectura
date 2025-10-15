b2f90104efb9362d83b1a20102bc49c25be74e22
mestepa12 <mestepa120504@gmail.com>
2025-10-15 13:01:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 const firebaseConfig = {
-  apiKey: "AIzaSyDGgrJwBRmz5hAqkgx3A6CnNRZuR_YtLfc",
+  apiKey: "AIzaSyDGgrJwBRmz5hAqkgx3A6CnNRZuR_YtLfc", // Pega aquí tu última clave de API
   authDomain: "mi-rincon-de-lectura.firebaseapp.com",
   projectId: "mi-rincon-de-lectura",
-  storageBucket: "mi-rincon-de-lectura.firebasestorage.app",
+  storageBucket: "mi-rincon-de-lectura.appspot.com",
   messagingSenderId: "333643518949",
   appId: "1:333643518949:web:322ec9b7ab1c3bc267d50c"
 };
-=======
-const firebaseConfig = {
-  apiKey: "AIzaSyDGgrJwBRmz5hAqkgx3A6CnNRZuR_YtLfc",
-  authDomain: "mi-rincon-de-lectura.firebaseapp.com",
-  projectId: "mi-rincon-de-lectura",
-  storageBucket: "mi-rincon-de-lectura.firebasestorage.app",
-  messagingSenderId: "333643518949",
-  appId: "1:333643518949:web:322ec9b7ab1c3bc267d50c"
-};
->>>>>>> 1d32ea9fbf6d995066301996d6ee4c01bcd723cb
